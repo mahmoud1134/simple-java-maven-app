afb7e410a6b647c7157d190de2c66bb124de8b58
mahmoud1134 <mm81641456789644@gmail.com>
2025-10-19 05:12:51 -0700

    First commit - Java Maven addition program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 5, b = 7;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
